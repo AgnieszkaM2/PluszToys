5197e755fbbcc0280b4f7031b5584e20abb1dfb3
Jarosław Smolira <j.smolira@netkoncept.com>
2023-05-06 16:16:58 +0200

    login form

--- Application/plusz-toys/src/components/Login.js
+++ Application/plusz-toys/src/components/Login.js
@@ -1,12 +1,30 @@
 import './HomeApp.css';
+import './Login.css';
+
+import userImg from '../assests/icons8-customer-32.png';
+import passImg from '../assests/icons8-password-key-24.png'
 
 export function Login() {
     return (
-        <>
-            <div className='app-wrap'>
-              <h1 align="center">Logowanie</h1>  
+        <><div className='app-wrap'><h1 align='center'>Logowanie</h1>
+
+            <div align='center'>
+            <form className='form' align='center'>
+                <div>
+                    <img src={userImg} alt="" className='icon user' />
+                    <input type="email" className='mainInputStyle input' placeholder="Email or Username" />
+                </div>
+                <div>
+                    <img src={passImg} alt="" className='icon pass' />
+                    <input type="password" className='mainInputStyle input' placeholder="Password" />
+                </div>
+                <button className='mainInputStyle loginBtn'>Login</button>
+            </form>
             </div>
-            
+
+        </div>
+
         </>
+
     )
 }
